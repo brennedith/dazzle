ac4b71535dd22bae6c11871e0364ebe04830cd93
Brennedith Garcia <brennedithg@gmail.com>
2018-01-08 09:14:30 -0600

    Deleted old services

--- src/modules/services/playbook.js
+++
@@ -1,9 +0,0 @@
-export default {
-  'Opening': ['Coming soon! Ideas are welcome :)'],
-  'Probing': ['Coming soon! Ideas are welcome :)'],
-  'Benefits': ['Coming soon! Ideas are welcome :)'],
-  'Offer': ['Coming soon! Ideas are welcome :)'],
-  'Close': ['Coming soon! Ideas are welcome :)'],
-  'Overcoming Objections': ['Coming soon! Ideas are welcome :)'],
-  'World Class Experience': ['Coming soon! Ideas are welcome :)']
-}
